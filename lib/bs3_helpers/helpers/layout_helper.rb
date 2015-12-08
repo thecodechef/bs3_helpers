d29c2dad8fa469f92a72e11d7244644a6c1b0790
Jeremy Bolding <cyberchefjay@gmail.com>
2015-12-08 17:47:13 -0600

    Fixed some build issues.

--- lib/bs3_helpers/helpers/layout_helper.rb
+++ lib/bs3_helpers/helpers/layout_helper.rb
@@ -15,16 +15,18 @@ module Bs3Helpers
 						instance_variable_set "@col_#{s}_#{@grids.fetch(3)}_#{i}", "#{@grids.fetch(0)}-#{s}-#{@grids.fetch(3)}-#{i}"
 					end
 				end
+				
 				@classes = []
 				
 				if opts[:full]
-					@classes << @col_xs_12 
+					@classes << instance_variable_get("@col_#{s}_#{i}") if opts[:size] == @sizes.fetch(i).to_sym 
 					@classes << @col_sm_12 
 					@classes << @col_md_12
 					@classes << @col_lg_12
 					@classes << @col_xl_12
 					content_tag :div,'',class: @classes.join(' '),&block
 				end
+
 			end
 		end
 	end
